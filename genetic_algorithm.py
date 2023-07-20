65a1c12e7b265a9f2053e8630adb2b26abcd6bff
Karl Davis <karldavis@me.com>
2023-07-20 14:05:54 +0100

    Update genetic_algorithm.py
    Added extra data outputs to help with dry-runs

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -13,13 +13,25 @@ def genetic_algorithm(constants, word):
     highest_fitness_score = float('-inf')
 
     for generation in range(constants['MAX_GENERATIONS']):
+        row_fitness = []
+        col_fitness = []
+        subgroup_fitness = []
         # Evaluate fitness for each individual in the population
-        fitness_scores = [evaluate_fitness(constants, individual, word) for individual in population]
+        fitness_scores = []
+        for individual in population:
+            fitness_score, row_score, col_score, subgroup_score = evaluate_fitness(constants, individual)
+            fitness_scores.append(fitness_score)
+            row_fitness.append(row_score)
+            col_fitness.append(col_score)
+            subgroup_fitness.append(subgroup_score)
 
         # Write fitness scores and get the highest fitness child
         highest_fitness_child, highest_fitness_score = write_fitness_scores_to_csv(fitness_scores,
                                                                                    population,
-                                                                                   'fitness_scores.csv')
+                                                                                   'fitness_scores.csv',
+                                                                                   row_fitness,
+                                                                                   col_fitness,
+                                                                                   subgroup_fitness)
 
         # Check termination condition
         if constants['MAX_FITNESS'] in fitness_scores:
@@ -43,26 +55,30 @@ def genetic_algorithm(constants, word):
         # Replace the population with mutated children
         population = mutated_children
 
-    return highest_fitness_child, highest_fitness_score
+    return highest_fitness_child, highest_fitness_score, row_score, col_score, subgroup_score
 
 
 def evaluate_fitness(constants, grid):
-    fitness = 0
+    row_score = 0
+    col_score = 0
+    subgroup_score = 0
 
     # Check rows
     for row in grid:
-        if len(row) == len(set(row)) and '-' not in row:
-            fitness += 1
+        row_words = [cell.lower() if cell != '-' else '-' for cell in row]
+        if len(row_words) == len(set(row_words)) and '-' not in row_words:
+            row_score += 1
         else:
-            fitness -= 1
+            row_score -= 1
 
     # Check columns
     for j in range(constants['GRID_SIZE']):
         column = [grid[i][j] for i in range(constants['GRID_SIZE'])]
-        if len(column) == len(set(column)) and '-' not in column:
-            fitness += 1
+        column_words = [cell.lower() if cell != '-' else '-' for cell in column]
+        if len(column_words) == len(set(column_words)) and '-' not in column_words:
+            col_score += 1
         else:
-            fitness -= 1
+            col_score -= 1
 
     # Check subgroups
     for i in range(0, constants['GRID_SIZE'], constants['SUBGRID_SIZE']):
@@ -76,11 +92,13 @@ def evaluate_fitness(constants, grid):
 
             if len(set(subgrid_letters)) == constants['SUBGRID_SIZE'] * constants['SUBGRID_SIZE'] \
                     and '-' not in subgrid_letters:
-                fitness += 4
+                subgroup_score += 4
             else:
-                fitness -= 4
+                subgroup_score -= 4
 
-    return fitness
+    fitness = row_score + col_score + subgroup_score
+
+    return fitness, row_score, col_score, subgroup_score
 
 
 def crossover(constants, parent1, parent2):
